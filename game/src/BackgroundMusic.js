9a24488ba5aca419f67985e816beab6a1244ff5f
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-03-18 19:44:54 +0200

    audio

--- game/src/BackgroundMusic.js
+++ game/src/BackgroundMusic.js
@@ -8,7 +8,7 @@ class BackgroundMusic extends Component {
     }
   }
 
-  playBackgroundMusic = () => {
+  backGroundMusic = () => {
     const music = document.getElementById('music');
     const pButton = document.getElementById('pButton');
     if (music.paused) {
@@ -28,7 +28,7 @@ class BackgroundMusic extends Component {
         <audio id="music" preload="true" src="audio/game_music.mp3" autoPlay/>
         <div>
           <p>Background music</p>
-        	<i id="pButton" className="fa fa-pause-circle" onClick={this.playBackgroundMusic}></i>
+        	<i id="pButton" className="fa fa-pause-circle" onClick={this.backGroundMusic}></i>
         </div>
       </div>
     )

dab90cf0b10239cf3ffc8f63156fb6b069b99d16
Niki Ahlskog <niki.ahlskog@gmail.com>
2018-03-24 14:24:22 +0200

    wallet

--- game/src/Config.js
+++ game/src/Config.js
@@ -2,4 +2,4 @@ export const defaultBlocks = 0
 
 export const blocksToCoin = 30
 
-export const defaultRate = 0.005
+export const defaultRate = 0.001
--- game/src/Wallet.js
+++ game/src/Wallet.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
-export default class Wallet extends Component {
+class Wallet extends Component {
   render() {
     return (
       <div className="menu-item">
         <div>
           <h3>Account Balance</h3>
-          <p>20 Trollcoins <i className="fa fa-connectdevelop"></i></p>
-          <p>400 $ <i className="fa fa-credit-card"></i></p>
+          <p>
+            <strong className="wallet-coins">{this.props.coins} </strong>
+            Trollcoins <i className="fa fa-connectdevelop"></i>
+          </p>
+          <p>0 $ <i className="fa fa-credit-card"></i></p>
         </div>
       </div>
     )
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    blocks: state.blocks,
+    coins: state.coins,
+    hashingRate: state.hashingRate,
+    assets: 0,
+    electricity: 0
+  }
+}
+
+export default connect(mapStateToProps)(Wallet)
